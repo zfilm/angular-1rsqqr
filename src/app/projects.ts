86c68b8185f0ab48270af107c288c6ec7f534917
zfilm <morten.foyn.boe@gmail.com>
2019-09-07 17:07:49 +0200

    2 files modified, 1 added

---
+++ src/app/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    name: 'Dokumentasjoningeniør',
+    company: 'Company 1',
+    completed: 80,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Produktutvikler',
+    company: 'Company 1',
+    completed: 30,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Avdelingsleder',
+    company: 'Company 2',
+    completed: 67,
+    description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.'
+  }
+];
